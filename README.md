7d91340c66a04a2648c51f0526668199ba21fe4a
charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>
2021-02-23 13:25:49 -0700

    feat: add installer.sh
    Signed-off-by: charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # aws-ec2-init
+
 Installs Docker and a few other basic dependencies needed in order to run an AWS EC2 instance my way.
--- installer.sh
+++ installer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xe
 
+git config --global credential.helper 'cache --timeout=604800'
+
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo usermod -aG docker $USER
 
-sudo apt update && sudo apt install -y docker-compose htop
+sudo apt update && sudo apt install -y docker-compose htop amazon-efs-utils
