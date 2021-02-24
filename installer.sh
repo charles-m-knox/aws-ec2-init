fc89e188b9632aa7335b3fe61bb73d5b7d8ceb58
charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>
2021-02-23 20:58:18 -0700

    feat: add kick-caddy alias
    Signed-off-by: charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>

--- installer.sh
+++ installer.sh
@@ -11,6 +11,7 @@ sudo apt update && sudo apt install -y docker-compose htop make
 # set up docker compose alias
 grep "alias dc=" ~/.bashrc || echo "alias dc='docker-compose'" >>~/.bashrc
 grep "alias kick=" ~/.bashrc || echo "alias kick='git pull && dc down && dc up -d && dc logs -f'" >>~/.bashrc
+grep "alias kick-caddy=" ~/.bashrc || echo "alias kick-caddy='git pull && docker rm -f caddy && dc up -d caddy && dc logs -f'" >>~/.bashrc
 
 # activates bashrc
 . ~/.bashrc
